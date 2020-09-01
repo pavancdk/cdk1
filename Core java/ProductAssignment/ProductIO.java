eef2a7f944815bfbf3d5a52544b685b5abd496b1
pavancdk <69522665+pavancdk@users.noreply.github.com>
2020-09-01 09:24:46 +0530

    Create ProductIO.java

---
+++ Core java/ProductAssignment/ProductIO.java
@@ -0,0 +1,30 @@
+package com.io;
+
+import java.util.Scanner;
+
+import com.pojo.Product;
+
+public class ProductIO {
+	static Scanner sc = new Scanner(System.in);
+	
+	public Product getProduct(){
+		// sc = new Scanner(System.in);
+		System.out.println("enter the product id :");
+		int pId=sc.nextInt();
+		System.out.println("enter the product name :");
+		String pName = sc.next().trim();
+		System.out.println("enter the product price :");
+		double price = sc.nextDouble();
+		System.out.println("enter the product quantity :");
+		int quantity= sc.nextInt();
+		System.out.println("enter the product category :");
+		String  category=sc.next().trim();
+		//sc.close();
+		return new Product(pId, pName, price, quantity, category);
+	}
+	public void displayProduct(Product product)
+	{
+		System.out.println(product);
+	}
+	
+}
